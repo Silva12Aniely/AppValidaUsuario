5c36a5b22eb2eeb688e05440554717d11e8304c5
aniely.osilva <anielysilva08@gmail.com>
2019-08-06 15:16:26 -0300

    06/08

--- app/src/main/java/com/example/appvalidausuario/MainActivity.java
+++ app/src/main/java/com/example/appvalidausuario/MainActivity.java
@@ -2,13 +2,53 @@ package com.example.appvalidausuario;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText txtUsuario, txtSenha;
+    Button btnEntrar, btnSair;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtUsuario = (EditText) findViewById(R.id.txtUsuario);
+        txtSenha = (EditText) findViewById(R.id.txtSenha);
+        btnEntrar = (Button) findViewById(R.id.btnEntrar);
+        btnSair = (Button) findViewById(R.id.btnSair);
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String usuario, senha;
+
+                usuario = txtUsuario.getText().toString();
+                senha = txtSenha.getText().toString();
+
+                if (usuario.equals("senac") && senha.equals("senac")) {
+                    startActivity(new Intent(getApplicationContext(), RespondeUsuario_Activity.class));
+
+                    //Intent intent = new Intent(getApplicationContext(), RespondeUsuario_Activity.class);
+                    //startActivity(intent);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Usuário ou senha inválida", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/appvalidausuario/RespondeUsuario_Activity.java
+++ app/src/main/java/com/example/appvalidausuario/RespondeUsuario_Activity.java
@@ -2,13 +2,25 @@ package com.example.appvalidausuario;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RespondeUsuario_Activity extends AppCompatActivity {
-
+    Button btnVoltar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.responde_usuario_layout);
+
+        btnVoltar = (Button) findViewById(R.id.btnVoltar);
+
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
     }
 }
