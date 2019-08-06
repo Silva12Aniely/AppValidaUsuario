608da428c237e2079639a3032d83dd316292475b
aniely.osilva <anielysilva08@gmail.com>
2019-08-06 13:55:38 -0300

    Initial commit

---
+++ app/src/main/java/com/example/appvalidausuario/RespondeUsuario_Activity.java
@@ -0,0 +1,14 @@
+package com.example.appvalidausuario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RespondeUsuario_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.responde_usuario_layout);
+    }
+}
